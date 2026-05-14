e80877a97f13a899c6b6645558077dd42f0b5ca8
Tanish Chopra <143783579+tanish-chopra@users.noreply.github.com>
2026-05-14 18:37:00 +0530

    Update 0004_remove_book_download_count_book_downloads.py

--- cources/migrations/0004_remove_book_download_count_book_downloads.py
+++ cources/migrations/0004_remove_book_download_count_book_downloads.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('courses', '0003_alter_book_description'),
+        ('cources', '0003_alter_book_description'),
     ]
 
     operations = [
