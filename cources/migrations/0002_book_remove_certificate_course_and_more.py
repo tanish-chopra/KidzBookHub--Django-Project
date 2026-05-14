8e0681a35b96277cdacb6314d7910240a7a77157
Tanish Chopra <143783579+tanish-chopra@users.noreply.github.com>
2026-05-14 18:36:30 +0530

    Update 0002_book_remove_certificate_course_and_more.py

--- cources/migrations/0002_book_remove_certificate_course_and_more.py
+++ cources/migrations/0002_book_remove_certificate_course_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('courses', '0001_initial'),
+        ('cources', '0001_initial'),
     ]
 
     operations = [
